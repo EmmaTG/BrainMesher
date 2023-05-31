37e2737ceaf45ab4285c7a9156777ecc1d7641ea
EmmaTG <grif.emma@gmail.com>
2023-05-31 15:09:04 +0200

    material mapping working for abq files, ucd still to be confirmed

--- BrainModel.py
+++ BrainModel.py
@@ -14,10 +14,6 @@ from Vertex import Vertex
 from GridBox import GridBox
 
 class BrainModel():
-    
-    def __init__(self):        
-        self.labelsMap = {}
-        self.inverseLabelsMap = {}
         
     def check_data_dims(self,data):        
         if len(data.shape) == 3:
@@ -25,59 +21,6 @@ class BrainModel():
         else:
             print('Error, data shape not 3 dimensional!')
             return False       
-        
-    def clear_labels_mapself(self):
-        self.labelsMap = {}
-        self.inverseLabelsMap = {}
-        
-    def addLabelToMap(self, name, numbers):
-        if not (hasattr(self, "labelsMap")):
-            self.create_labels_map();
-        
-        storedLabelValues = []
-        for x in list(self.labelsMap.values()):
-            storedLabelValues += list(x)
-        labelsArr = []
-        if (self.labelsMap.__contains__(name)):
-            labelsArr = self.labelsMap[name]
-        else:
-            self.labelsMap[name] = labelsArr;          
-        try:    
-            if (isinstance(numbers, (collections.abc.Sequence, np.ndarray))):
-                for num in numbers:                    
-                    num = int(num)
-                    if storedLabelValues.count(num):
-                        self.labelsMap.pop(name)
-                        print("Error. The value '{}' already exists in the map".format(num))
-                        return -1
-            else:
-                numbers = [int(numbers)]
-        except ValueError or TypeError:
-            print("Error: input number '{}' not compatible".format(numbers))
-            self.labelsMap.pop(name)
-            return -1
-            
-        
-        labelsArr += list(numbers);
-        for n in numbers:
-            self.inverseLabelsMap[n] = name
-        return 0;
-
-    def homogenize_labels(self, data):
-        print("Homogenizing data according to chosen labels")
-        current_dimensions = data.shape
-        newData = np.zeros(current_dimensions, int)
-        for x in range(current_dimensions[0]):
-            if (np.sum(data[x,:,:]) > 0):
-                for y in range(current_dimensions[1]):
-                    if (np.sum(data[x,y,:]) > 0):
-                        for z in range(current_dimensions[2]):
-                            data_value = data[x,y,z]
-                            if self.inverseLabelsMap.__contains__(data_value):
-                                label_name = self.inverseLabelsMap[data_value]
-                                label_number = self.labelsMap[label_name][0]
-                                newData[x,y,z] = label_number
-        return newData;        
     
     def coarsen(self, new, original_data):        
         print("Coarsening mesh by a factor of " + str(new))
